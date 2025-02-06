3e8708e9fa82022d87f95b8ab14c4d06faf28977
Vitaliy Gorbenko <vitaliy.gorbenko@edge10group.com>
2025-02-06 21:07:06 +0700

    Added stay warm trigger

---
+++ src/Cloud5mins.ShortenerTools.Functions/Functions/StayWarmTrigger.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+
+namespace Cloud5mins.ShortenerTools.Functions;
+
+public static class StayWarmTrigger
+{
+	[Function("StayWarmTrigger")]
+	public static void RunAsync([TimerTrigger("0 */10 * * * *")] TimerInfo myTimer, FunctionContext context)
+	{
+		var log = context.GetLogger(nameof(StayWarmTrigger));
+
+		log.LogInformation("Executed timer trigger to keep function app warm.");
+	}
+}
